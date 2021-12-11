171e3c4501cfc13f4bfac29fea3ea454300c37a1
Tarwin3v <tarwin.ev@gmail.com>
2021-12-11 18:21:54 +0100

    covid component

---
+++ Uber_clone/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+import HomeScreen from './ui/screens/HomeScreen';
+
+const App = () => {
+  return <HomeScreen />;
+};
+
+export default App;
