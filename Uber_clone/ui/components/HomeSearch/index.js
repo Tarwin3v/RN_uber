73b34c831342058069cca46c30a2758f38f603a9
Tarwin3v <tarwin.ev@gmail.com>
2021-12-13 14:23:53 +0100

    home search ui

--- Uber_clone/ui/components/HomeSearch/index.js
+++ Uber_clone/ui/components/HomeSearch/index.js
@@ -1,8 +1,35 @@
 import React from 'react';
-import {View} from 'react-native';
+import {Text, View} from 'react-native';
+import styles from './styles';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import Entypo from 'react-native-vector-icons/Entypo';
 
 const HomeSearch = () => {
-  return <View></View>;
+  return (
+    <View>
+      <View style={styles.inputField}>
+        <Text style={styles.inputText}>HomeSearch</Text>
+        <View style={styles.timeContainer}>
+          <AntDesign name="clockcircle" size={16} color="#535353" />
+          <Text style={{color: '#535353'}}>Now</Text>
+          <MaterialIcons name="keyboard-arrow-down" size={16} color="#535353" />
+        </View>
+      </View>
+      <View style={styles.row}>
+        <View style={styles.iconContainer}>
+          <AntDesign name="clockcircle" size={16} color="#fff" />
+        </View>
+        <Text style={styles.destinationText}>Spin Nightclub</Text>
+      </View>
+      <View style={styles.row}>
+        <View style={[styles.iconContainer, {backgroundColor: '#218cff'}]}>
+          <Entypo name="home" size={20} color="#fff" />
+        </View>
+        <Text style={styles.destinationText}>Spin Nightclub</Text>
+      </View>
+    </View>
+  );
 };
 
 export default HomeSearch;
